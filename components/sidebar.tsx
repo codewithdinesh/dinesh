36bb82c76dea0f9e2a8d3981c9b3e060fb3452ae
Dinesh <pr760677@gmail.com>
2024-07-05 23:00:10 +0530

    Project Initiallized

---
+++ components/sidebar.tsx
@@ -0,0 +1,13 @@
+// components/Sidebar.js
+import React from 'react';
+import { Button, Link, Spacer } from '@nextui-org/react';
+
+const Sidebar = () => {
+    return (
+
+    
+      
+    );
+};
+
+export default Sidebar;
---
+++ config/site.ts
@@ -0,0 +1,70 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Dinesh Rathod",
+  description: "Full stack Web Developer",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+    {
+      label: "Experience",
+      href: "/experience",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Contact",
+      href: "/contact",
+    },
+
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/codewithdinehs",
+    twitter: "https://twitter.com/codewithdinesh",
+    instagram: "https://instagram.com/dineshrathod.ai",
+    linkedin: "https://linkedin.com/in/dineshrathod03"
+
+  },
+};
