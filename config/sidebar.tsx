4e678c264c530e92a76a356ba372a24a0167523a
Dinesh Rathod <72983957+codewithdinesh@users.noreply.github.com>
2024-10-05 21:37:28 +0530

    Update Config Data

--- config/sidebar.tsx
+++ config/sidebar.tsx
@@ -8,14 +8,7 @@ export const UserDetails = {
         github: "https://www.github.com/codewithdinesh",
         linkedin: "https://www.linkedin.com/in/dineshrathod03",
         twitter: "https://www.twitter.com/codewithdinesh",
-        instagram: "https://www.instagram.com/dineshrathod.ai",
-        discord: "https://discord.gg/8uJ8J2b",
-        huggingface: "https://huggingface.co/dineshrathod",
-        kaggle: "https://www.kaggle.com/dineshrathod",
-        medium: "https://dineshrathod.medium.com",
-        dev: "https://dev.to/dineshrathod",
-        youtube: "https://www.youtube.com/c/codewithdinesh",
-
+        instagram: "https://www.instagram.com/dineshrathod.ai"   
     },
 
 }
